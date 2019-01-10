2e76961c36106bbfda07728763278bf7010cdb29
charlesstoll <charlesrstoll@gmail.com>
2019-01-10 12:40:45 -0800

    Initial commit
    Initial commit with a simple hello world script and a fact file that can
    be queried.

---
+++ code/simple_facts.pl
@@ -0,0 +1,8 @@
+likes(mary,food).
+likes(mary,wine).
+likes(john,wine).
+likes(john,mary).
+
+/* likes is not a special operator. just a generic relation that we define */
+eats(mary,potato).
+drinks(mary,juice).
