787e12a73accaa030cc6002dc2b8ec5867a081e3
charlesstoll <charlesrstoll@gmail.com>
2019-01-17 12:56:43 -0800

    Added the half sibling function as an example
    This adds a half sibling function to family.pl which gives a good
    example of complex relationships which involve nots.

--- code/family.pl
+++ code/family.pl
@@ -9,6 +9,7 @@ is_female(susan).
 is_female(samantha).
 is_female(kate).
 is_female(becky).
+is_female(mary).
 
 /* some relations */
 is_parent(jenny, john).
@@ -17,6 +18,7 @@ is_parent(charles, john).
 is_parent(susan, samantha).
 is_parent(mark,samantha).
 
+is_parent(mary, kate).
 is_parent(mark,kate).
 
 is_parent(samantha,becky).
@@ -34,5 +36,8 @@ is_father(X, Y) :- is_male(X), is_parent(X, Y).
 is_not_mother(X, Y) :- is_mother(X, Y).
 has_same_mother(X, Y) :- is_mother(Z, X), is_mother(Z, Y).
 has_same_father(X, Y) :- is_father(Z, X), is_father(Z, Y).
+has_diff_mother(X, Y) :- is_mother(Z, X), is_mother(W, Y), Z \= W.
+has_diff_father(X, Y) :- is_father(Z, X), is_father(W, Y), Z \= W.
 is_full_sibling(X, Y) :- has_same_father(X, Y), has_same_mother(X, Y), X\= Y.
+is_half_sibling(X, Y) :- has_diff_mother(X, Y), has_same_father(X, Y) ; has_diff_father(X, Y), has_same_mother(X, Y).
 are_married(X, Y) :- p_are_married(X, Y) ; p_are_married(Y, X).
